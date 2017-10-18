72941f5818c8838ccad4fc29acac43dc0d10f01d
javy_liu <javy_liu@163.com>
2017-10-18 18:58:40 +0800

    add new pics

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -10,11 +10,28 @@ import './lib/foundation-explicit-pieces';
 
 
 // $(document).foundation();
-
+function resize() {
+    var h = $(window).height();
+    var w = $(window).width();
+    var rate = w / h;
+    // console.log(rate);
+    if (rate < 1.78) {
+        $("video").width(h * 1.8);
+    } else {
+        $("video").width(w);
+    }
+}
 $(function() {
+    // console.log("video loaded");
+
     // var _videoHeight = $("video").height();
     // 
     // $(".mask img").height(_videoHeight);
-
+    // $("video").on("load", function() {
+    //     console.log("video loaded");
+    // })
+    resize();
 
 });
+
+$(window).resize(resize);
